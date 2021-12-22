4142b28f6c54d4f2e91da0edb5395e9f25e88a64
Takafumi Moriyama <taka.youthful.426@gmail.com>
2021-12-23 08:51:00 +0900

    開発_Webルーティング追加

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\RecipeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes');
