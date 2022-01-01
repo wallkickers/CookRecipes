ddc42ef6c127c3189f9aa5ce7f4436b4c789f131
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-01-01 20:27:40 +0900

    開発_レイアウトファイル追加

---
+++ backend/resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="description" content="ホームページサンプル株式会社 | ホームページサンプル株式会社では最新技術と自然との調和を目指します。">
+        <meta name="keywords" content="">
+        <title>ホームページサンプル株式会社 | ホームページサンプル株式会社では最新技術と自然との調和を目指します。</title>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="screen">
+        @livewireStyles
+    </head>
+    <body>
+        <!-- ヘッダー -->
+        <div id="header">
+            <div class="inner">
+                <div class="logo">
+                    <a href="{{ route('recipes') }}"></a>
+                </div>
+            </div>
+        </div>
+        <!-- / ヘッダー -->
+
+        @yield('content')
+
+        <!-- フッター -->
+        <div id="footer">
+            <div class="inner">
+            <section class="gridWrapper">
+                <article class="grid copyright">
+                    Copyright(c) 2012 Sample Inc. All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a>
+                </article>
+            </section>
+            </div>
+        </div>
+        @livewireScripts
+    </body>
+</html>
