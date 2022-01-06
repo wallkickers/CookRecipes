89ef9d82010373bad0f064ce09461f471ac68906
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-01-07 08:35:24 +0900

    開発_ドメインモデル追加

--- backend/packages/Domain/Web/Recipe.php
+++ backend/packages/Domain/Web/Recipe.php
@@ -2,30 +2,19 @@
 
 declare(strict_types=1);
 
-namespace packages\Domain\Api;
+namespace packages\Domain\Web;
 
 class Recipe
 {
-    private int $userId;
     private string $recipeUrl;
     private string $recipeTitle;
 
     public function __construct(
-        int $userId,
         string $recipeUrl,
-        string $recipeTitle
+        string $recipeTitle,
     ) {
-        $this->userId = $userId;
         $this->recipeUrl = $recipeUrl;
         $this->recipeTitle = $recipeTitle;
-    }
-
-    /**
-     * @return int
-     */
-    public function getUserId(): int
-    {
-        return $this->userId;
     }
 
     /**
