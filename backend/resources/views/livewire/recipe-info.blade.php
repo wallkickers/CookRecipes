9264848ee813950690097a64cfc7e8fc02465b15
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-01-10 17:08:38 +0900

    開発_レシピ一覧画面デザイン修正

--- backend/resources/views/livewire/recipe-info.blade.php
+++ backend/resources/views/livewire/recipe-info.blade.php
@@ -1,5 +1,6 @@
 <article class="grid">
-    <p class="img"><img src="images/eyecatch2.jpg" width="220" height="160" alt=""></p>
-    <h3>{{ $recipe->getRecipeUrl() }}</h3>
-    <h3>{{ $recipe->getrecipeTitle() }}</h3>
+    <a href='{{ $recipe->getRecipeUrl() }}'>
+        <p class="img"><img src="images/eyecatch2.jpg" width="220" height="160" alt=""></p>
+        <h3 class="RecipeTitle">{{ $recipe->getrecipeTitle() }}</h3>
+    </a>
 </article>
