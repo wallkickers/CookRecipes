5bf36dd8bbae83ff640b1d3eab222061e4c23a84
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-01-15 19:43:26 +0900

    開発_AppServiceProviderにインターフェイス記述

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Api
         $this->app->bind(
             \packages\Api\Recepes\Recipe\RecipeUsecaseInterface::class,
             \packages\Api\Recepes\Recipe\RecipeInteractor::class
@@ -21,6 +22,27 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             \packages\Api\Recepes\Recipe\RecipeRepositoryInterface::class,
             \packages\Api\Recepes\Recipe\RecipeRepository::class
+        );
+
+        $this->app->bind(
+            \packages\Api\Recepes\RecipeCreate\RecipeCreateUsecaseInterface::class,
+            \packages\Api\Recepes\RecipeCreate\RecipeCreateInteractor::class
+        );
+
+        // Web
+        $this->app->bind(
+            \packages\Web\Recepes\Recipe\RecipeUsecaseInterface::class,
+            \packages\Web\Recepes\Recipe\RecipeInteractor::class
+        );
+
+        $this->app->bind(
+            \packages\Web\Recepes\Recipe\RecipeRepositoryInterface::class,
+            \packages\Web\Recepes\Recipe\RecipeRepository::class
+        );
+
+        $this->app->bind(
+            \packages\Web\Recepes\RecipeCreate\RecipeCreateUsecaseInterface::class,
+            \packages\Web\Recepes\RecipeCreate\RecipeCreateInteractor::class
         );
     }
 
