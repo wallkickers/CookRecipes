89bbf85983e1c8a6dd645068673896666aad55c7
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-01-21 22:02:09 +0900

    開発_レシピ作成_Interactor実装

---
+++ backend/packages/Web/Recipes/RecipeCreate/RecipeCreateInteractor.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace packages\Web\Recepes\RecipeCreate;
+
+use packages\Web\Recepes\Recipe\RecipeRepositoryInterface;
+use packages\Web\Recepes\RecipeCreate\RecipeCreateRequest;
+use packages\Web\Recepes\RecipeCreate\RecipeCreateResponse;
+use packages\Web\Recepes\RecipeCreate\RecipeCreateUsecaseInterface;
+
+class RecipeCreateInteractor implements RecipeCreateUsecaseInterface
+{
+    protected RecipeRepositoryInterface $recipeRepository;
+
+    public function __construct(
+        RecipeRepositoryInterface $recipeRepository
+    ) {
+        $this->recipeRepository = $recipeRepository;
+    }
+
+    /**
+     * @param RecipeCreateRequest
+     * @return RecipeCreateResponse
+     */
+    public function handle(RecipeCreateRequest $request): RecipeCreateResponse
+    {
+        $result = $this->recipeRepository
+            ->insertOrUpdate(
+                $request->getUserId(),
+                $request->getRecipeUrl(),
+                $request->getRecipeIngredient()
+            );
+        return new RecipeCreateResponse($result);
+    }
+}
