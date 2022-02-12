f5b9d3f5525d8d68bf2f0351f1d31ef786b4031d
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-02-12 20:25:54 +0900

    開発_レシピ作成_view修正

--- backend/resources/views/recipe_create.blade.php
+++ backend/resources/views/recipe_create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<form id="contact" route="{{ route('recipe_create_update') }}" method="POST">
+<form id="contact" action="{{ route('recipe_create_update') }}" method="POST">
     @csrf
     <div>
         <h2 class="Page-Title">レシピ作成</h2>
@@ -13,15 +13,17 @@
         </div>
         <div class="Form-Item">
           <p class="Form-Item-Label isMsg">材料</p>
-          <textarea class="Form-Item-Textarea" name="recipe_ingredients"
+          <textarea
+            class="Form-Item-Textarea"
+            name="recipe_ingredients"
             placeholder="例）鶏もも肉1パック(2枚500g)
             ★醤油大さじ4
             ★みりん大さじ3
             ★料理酒大さじ3
             ★砂糖大さじ1
             ★しょうがチューブ5〜10cm位
-            ★ニンニクチューブ5〜10cm位">
-            </textarea>
+            ★ニンニクチューブ5〜10cm位"
+            ></textarea>
         </div>
         <input type="submit" class="Form-Btn" value="保存する">
     </div>
