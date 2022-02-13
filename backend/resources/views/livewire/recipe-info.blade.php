37a4b0a4844f471ef3731a9ff5458f51eb52d255
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-02-13 09:43:44 +0900

    開発_レシピ一覧作成_リンク先URL修正

--- backend/resources/views/livewire/recipe-info.blade.php
+++ backend/resources/views/livewire/recipe-info.blade.php
@@ -1,5 +1,5 @@
 <article class="grid">
-    <a href='{{ $recipe->getRecipeUrl() }}'>
+    <a href='{{ route('recipe_detail', $recipe->getId() ) }}'>
         <p class="img"><img src="images/eyecatch2.jpg" width="220" height="160" alt=""></p>
         <h3 class="RecipeTitle">{{ $recipe->getrecipeTitle() }}</h3>
     </a>
