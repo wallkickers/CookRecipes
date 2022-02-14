49ef9e04c90b5de31b5b698bb3804ba9055361bc
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-02-14 09:33:26 +0900

    DB_recipe_ingredients_category_idカラムをnull許可に変更

--- backend/database/migrations/2021_12_21_093325_recipe_ingredients.php
+++ backend/database/migrations/2021_12_21_093325_recipe_ingredients.php
@@ -16,7 +16,7 @@ class RecipeIngredients extends Migration
         Schema::create('recipe_ingredients', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('recipe_id')->unsigned();
-            $table->unsignedBigInteger('ingredient_category_id')->unsigned();
+            $table->unsignedBigInteger('ingredient_category_id')->nullable()->unsigned();
             $table->string('ingredient_name');
             $table->string('ingredient_amount')->nullable()->default(null);
         });
