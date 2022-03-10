bfb6198989be70e576b8ea45ae3e50bd5df8cd1e
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-03-10 09:35:30 +0900

    開発_repository_interface修正

--- backend/packages/Web/Recipes/Recipe/RecipeRepositoryInterface.php
+++ backend/packages/Web/Recipes/Recipe/RecipeRepositoryInterface.php
@@ -8,11 +8,23 @@ interface RecipeRepositoryInterface
 {
     public function find(string $userId);
 
-    public function insertOrUpdate(
+    public function insert(
         string $userId,
         string $recipeUrl,
         string|null $recipeIngredient
     );
 
+    public function update(
+        string $recipeId,
+        string $userId,
+        string|null $recipeTitle,
+        string $recipeUrl,
+        array|null $recipeIngredients
+    );
+
     public function getRecipe(string $userId, string $recipeId);
+
+    public function getRecipeIngredients(string $recipeId);
+
+    public function getIngredientCategories();
 }
