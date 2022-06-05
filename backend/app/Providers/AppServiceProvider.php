e85a6b2a0df4434e282cf7aed4635fed367d6842
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-06-05 20:08:22 +0900

    デプロイ_prod環境ではhttps接続強制

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -101,5 +101,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'prod') {
+            URL::forceScheme('https');
+        }
     }
 }
