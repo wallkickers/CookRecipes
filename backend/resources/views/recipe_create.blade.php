6cd3b60cc21c9a6a2383c8cdd6fb8bc4e5d44e4c
Takafumi Moriyama <taka.youthful.426@gmail.com>
2022-06-17 09:39:37 +0900

    開発_レシピ作成画面追加

--- backend/resources/views/recipe_create.blade.php
+++ backend/resources/views/recipe_create.blade.php
@@ -1,12 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.login_app')
 
 @section('content')
-<form id="contact" action="{{ route('recipe_create_update') }}" method="POST">
+<form id="contact" action="{{ route('recipe_insert') }}" method="POST">
     @csrf
     <div>
         <h2 class="Page-Title">レシピ作成</h2>
     </div>
     <div class="Form">
+        <div class="Form-Item">
+            <p class="Form-Item-Label">レシピタイトル</p>
+            <input type="text" class="Form-Item-Input" name="recipe_title" placeholder="例）親子丼" required>
+          </div>
         <div class="Form-Item">
           <p class="Form-Item-Label">レシピURL</p>
           <input type="text" class="Form-Item-Input" name="recipe_url" placeholder="例）https://cookrecipe.com/aaaaa" required>
